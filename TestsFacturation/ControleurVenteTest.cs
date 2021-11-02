9cca61db3a69592605c84007279969ca87777a8a
tchouin <chouinard.tomy@carrefour.cegepvicto.ca>
2021-11-02 15:10:40 -0400

    ajout test inutile

---
+++ TestsFacturation/ControleurVenteTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Facturation;
+
+namespace TestsFacturation
+{
+    [TestClass]
+    public class ControleurVenteTest
+    {
+        private ControleurVente controleur;
+
+        [TestInitialize]
+        public void InitialisationTests()
+        {
+            controleur = new ControleurVente();
+        }
+
+        [TestMethod]
+        public void DemarrerVenteTest()
+        {
+            try { 
+                controleur.DemarrerVente();
+            }
+            catch(Exception e)
+            {
+                Assert.Fail("L'exécution de la méthode a échoué! Erreur: " + e);
+            }
+        }
+    }
+}
